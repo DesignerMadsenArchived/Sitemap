c51011dc8374d3e4cab28201adc73d64e072674b
Kent vejrup Madsen <kent.vejrup.madsen@designermadsen.dk>
2023-01-21 11:11:34 +0100

    added packages

--- code/Domain/SitemapEntry.php
+++ code/Domain/SitemapEntry.php
@@ -9,10 +9,31 @@
 	 */
 	class SitemapEntry
 	{
+		// Constructor
 		public function __construct()
 		{
 		
 		}
-	
+		
+		
+		// Variables
+		private ?string $url = null;
+		
+		// Accessors
+		/**
+		 * @return string|null
+		 */
+		public function getUrl(): ?string
+		{
+			return $this->url;
+		}
+		
+		/**
+		 * @param string|null $url
+		 */
+		public function setUrl( ?string $url ): void
+		{
+			$this->url = $url;
+		}
 	}
 ?>
--- code/Test/Test.php
+++ code/Test/Test.php
@@ -2,7 +2,7 @@
 	/**
 	 *
 	 */
-	namespace IoJaegers\Sitemap;
+	namespace IoJaegers\Sitemap\Test;
 	
 	/**
 	 *
